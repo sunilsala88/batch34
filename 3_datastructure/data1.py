dd7d10150761d0a9a3c0317c402f392b581f3788
sunilsala88 <sunilsala88@gmail.com>
2025-09-28 18:29:59 +0530

    first commit

---
+++ 3_datastructure/data1.py
@@ -0,0 +1,16 @@
+
+#list is a datastructure /datatype
+d1=[11,22,33,44,'hello',True,5.5]
+print(d1)
+
+#indexing and slicing
+print(d1[-1])
+print(d1[0:3])
+
+#list is mutable
+#add elem in a list
+d1.append(60)
+print(d1)
+
+d1.insert(1,88)
+print(d1)
