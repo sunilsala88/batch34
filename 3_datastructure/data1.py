5232aae452c064b0db38a3e15254c205deaecc59
sunilsala88 <sunilsala88@gmail.com>
2025-09-28 18:49:47 +0530

    sep-28-2

--- 3_datastructure/data1.py
+++ 3_datastructure/data1.py
@@ -1,33 +1,44 @@
 
-#list is a datastructure /datatype
-d1=[11,22,33,44,'hello',True,5.5]
-print(d1)
+# #list is a datastructure /datatype
+# d1=[11,22,33,44,'hello',True,5.5]
+# print(d1)
+
+# #indexing and slicing
+# print(d1[-1])
+# print(d1[0:3])
+
+# #list is mutable
+# #add elem in a list
+# d1.append(60)
+# print(d1)
+
+# d1.insert(1,88)
+# print(d1)
+
+# #remove element 
+# d1.remove(5.5)
+# print(d1)
+
+# d1.pop(2)
+# print(d1)
+
+# #old approach
+# del d1[1]
+# print(d1)
+
+# d1[1]=99
+# print(d1)
+
+# i=d1.index('hello')
+# print(i)
+
+
+t1=(33,44,55,66)
+print(t1)
 
 #indexing and slicing
-print(d1[-1])
-print(d1[0:3])
+print(t1[0])
+print(t1[:2])
 
-#list is mutable
-#add elem in a list
-d1.append(60)
-print(d1)
-
-d1.insert(1,88)
-print(d1)
-
-#remove element 
-d1.remove(5.5)
-print(d1)
-
-d1.pop(2)
-print(d1)
-
-#old approach
-del d1[1]
-print(d1)
-
-d1[1]=99
-print(d1)
-
-i=d1.index('hello')
+i=t1.index(66)
 print(i)
