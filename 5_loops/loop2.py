55596842aea94fa4a5f1f3c24e261e8f925cc5fe
sunilsala88 <sunilsala88@gmail.com>
2025-10-11 18:28:12 +0530

    sat-11-1

---
+++ 5_loops/loop2.py
@@ -0,0 +1,15 @@
+
+
+#type 4
+stock_prices={'AAPL': 150, 'GOOGL': 2800, 'AMZN': 3400}
+
+# for stock in stock_prices:
+#     print(stock)
+#     print(stock_prices[stock])
+
+print(list(stock_prices.keys()))
+print(list(stock_prices.values()))
+print(list(stock_prices.items()))
+
+for i,j in stock_prices.items():
+    print(i,j)
