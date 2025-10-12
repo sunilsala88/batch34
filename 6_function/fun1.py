e6f8b0f6e7009127c0fe1a86c709c25ab0ac40e8
sunilsala88 <sunilsala88@gmail.com>
2025-10-13 00:13:08 +0530

    oct-13-1

--- 6_function/fun1.py
+++ 6_function/fun1.py
@@ -27,6 +27,47 @@ print(b)
 c=average(44)
 print(c)
 
+def get_fib(num:int)->list:
+
+    fib=[1,1]
+    num1=fib[0]
+    num2=fib[1]
+    for i in range(num-2):
+        num3=num1+num2
+        fib.append(num3)
+        num1=num2
+        num2=num3
+    return fib
 
 ans=get_fib(10)
-[1,1,3,]
+print(ans)
+
+def reverse_list(list1:list)->list:
+
+    index1=-1
+    rev=[]
+    while True:
+        rev.append(list1[index1])
+        index1=index1-1
+
+        if index1<-len(list1):
+            break
+    return rev
+
+l2=reverse_list([1,2,3])
+print(l2)
+
+
+l1=[33,44,55]
+#-1,-2,-3
+
+i=-1
+while True:
+        
+    print(i)
+    i=i-1
+    if i==-4:
+        break
+
+
+print(l1[-3])
