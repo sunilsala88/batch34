8870a7744107538eb0a5b4b7b1b89398e1340988
sunilsala88 <sunilsala88@gmail.com>
2025-10-18 19:13:51 +0530

    oct-18-4

--- 6_function/problem1.py
+++ 6_function/problem1.py
@@ -1,21 +1,28 @@
 
 
 stock_prices={'AAPL': 890, 'GOOGL': 2800, 'AMZN': 3400,'NVDA':789}
-porfolio=[]
 
-while True:
-    name=input('enter the stock name (type q to quit)')
 
-    if name.lower()=='q':
-        break
-    if name=='NVDA':
-        print('this stock is not tradeble try some thing else')
-        continue
-    
-    found=stock_prices.get(name)
-    if found:
-        porfolio.append(name)
-    else:
-        print('stock name is invalid try again')
 
-print(porfolio)
+def take_input(stock_prices):
+    porfolio={}
+
+    while True:
+        name=input('enter the stock name (type q to quit)')
+
+        if name.lower()=='q':
+            break
+        if name=='NVDA':
+            print('this stock is not tradeble try some thing else')
+            continue
+        
+        found=stock_prices.get(name)
+        if found:
+            porfolio.update({name:found})
+        else:
+            print('stock name is invalid try again')
+
+    return (porfolio)
+
+p=take_input(stock_prices)
+print(p)
