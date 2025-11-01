8f9e1415b776d505ec1045cddaa937a57a8fbccc
sunilsala88 <sunilsala88@gmail.com>
2025-11-01 19:09:40 +0530

    nov1-3

--- 9_module/demo.py
+++ 9_module/demo.py
@@ -5,3 +5,16 @@ print('hello')
 time.sleep(1)
 
 print('world')
+
+import random
+
+#generate random num between 0 and 1
+print(random.random())
+print(random.randint(10,20))
+
+import os
+print(os.getcwd())
+
+import sys
+sys.exit()
+print('last line')
