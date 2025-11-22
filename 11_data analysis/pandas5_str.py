a8a7f35ebd265f84c9f1a3d965cb9b9f655fc8dd
sunilsala88 <sunilsala88@gmail.com>
2025-11-22 17:04:17 +0530

    sat-22-2

--- 11_data analysis/pandas5_str.py
+++ 11_data analysis/pandas5_str.py
@@ -15,3 +15,22 @@ print(df['Name'])
 print(df['Name'].str.replace(' ','-'))
 
 #diff
+temp_list=[]
+for i in df['Name']:
+    temp_list.append(i.replace(' ','-'))
+df['Name']=temp_list
+print(df)
+
+df1=pd.read_csv('/Users/algo trading 2025/batch34/11_data analysis/unicorn.csv')
+print(df1)
+
+l1=[]
+for i in df1['Funding']:
+    d=str(i)
+    l1.append(d)
+
+df1['new']=l1
+print(df1)
+
+df1['Valuation']=df1['Valuation'].astype('str')
+print(df1.info())
