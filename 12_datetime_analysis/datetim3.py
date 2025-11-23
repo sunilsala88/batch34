ab71e73ba02ffa931aecc35fb0b9e37efb62cded
sunilsala88 <sunilsala88@gmail.com>
2025-11-23 17:25:36 +0530

    nov-23-5

--- 12_datetime_analysis/datetim3.py
+++ 12_datetime_analysis/datetim3.py
@@ -6,6 +6,19 @@ df=pd.read_csv('/Users/algo trading 2025/batch34/12_datetime_analysis/sbin.csv')
 print(df)
 
 df['date']=pd.to_datetime(df['date'])
+
+
+tz1='Asia/Kolkata'
+tz2='US/Eastern'
+#add a time zone
+df['date']=df['date'].dt.tz_localize(tz=tz1)
+print(df)
+#remove a timezone
+# df['date']=df['date'].dt.tz_localize(tz=None)
+
+#convert timezone
+df['date']=df['date'].dt.tz_convert(tz=tz2)
+
 df=df.set_index('date')
 print(df)
 print(df.info())
