29f14606fe0a72f8c3cb91a1b01a9035281b8557
sunilsala88 <sunilsala88@gmail.com>
2025-11-24 12:01:13 +0530

    nov-24

--- 12_datetime_analysis/timezone1.py
+++ 12_datetime_analysis/timezone1.py
@@ -1,7 +1,11 @@
-import datetime as dt
-import pytz
-tz1=pytz.timezone('Asia/Kolkata')
-tz2=pytz.timezone('US/Eastern')
-t1=dt.datetime.now(tz=tz2)
-print(t1)
+# import datetime as dt
+# import pytz
+# tz1=pytz.timezone('Asia/Kolkata')
+# tz2=pytz.timezone('US/Eastern')
+# t1=dt.datetime.now(tz=tz2)
+# print(t1)
+
+import pendulum as dt
+p1=dt.now(tz='Asia/Kolkata')
+print(p1)
 
