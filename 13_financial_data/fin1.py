399a8507868ed27f6e370cb4f7998908a056f8c1
sunilsala88 <sunilsala88@gmail.com>
2025-11-30 18:18:48 +0530

    nov-30-4

--- 13_financial_data/fin1.py
+++ 13_financial_data/fin1.py
@@ -24,6 +24,9 @@ import yfinance as yf
 # print(data)
 
 
-data=yf.download(['EURUSD=X'],period='5d',multi_level_index=False,interval='1m',ignore_tz=True)
-print(data)
+# data=yf.download(['EURUSD=X'],period='100d',multi_level_index=False,interval='1h',ignore_tz=True)
+# print(data)
 
+import datetime as dt
+data=yf.download(['GOOG'],start=dt.datetime(2021,1,1),end=dt.datetime(2021,12,31),multi_level_index=False)
+print(data)
